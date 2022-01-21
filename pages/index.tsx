2fac91735385a7207309bbd596ddab4c1a1aed28
jacknifesoulde6 <jacknifesoulde6@gmail.com>
2022-01-21 15:18:24 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,29 +4,31 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   // 「Unhandled error」の場合
-  React.useEffect(() => {
-    // const res = fetch("http://localhost:3001");
-    const func = async () => {
-      try {
-        // const res = await fetch("http://localhost:3000/api/hello");
-        const res = await fetch(
-          "https://next-app-three-rose.vercel.app/api/hello"
-        );
-        console.log(res);
-        // const a = { b: "あいう" };
-        // console.log(a.c.toString());
-      } catch (error) {
-        console.error(error);
-        // throw new Error("");
-      }
-    };
-    func();
-  }, []);
+  // React.useEffect(() => {
+  //   // const res = fetch("http://localhost:3001");
+  //   const func = async () => {
+  //     try {
+  //       // const res = await fetch("http://localhost:3000/api/hello");
+  //       const res = await fetch(
+  //         "https://next-app-three-rose.vercel.app/api/hello"
+  //       );
+  //       console.log(res);
+  //       // const a = { b: "あいう" };
+  //       // console.log(a.c.toString());
+  //     } catch (error) {
+  //       console.error(error);
+  //       // throw new Error("");
+  //     }
+  //   };
+  //   func();
+  // }, []);
 
   // const a = { b: "あいう" };
   // console.log(a.c.toString());
+
+  console.log("props", props);
 
   return (
     <div className={styles.container}>
@@ -92,5 +94,14 @@ const Home: NextPage = () => {
     </div>
   );
 };
+
+export async function getStaticProps() {
+  // const res = await fetch("http://localhost:3000/api/hello");
+  const res = await fetch("https://next-app-three-rose.vercel.app/api/hello");
+  const jsons = await res.json();
+  return {
+    props: { jsons },
+  };
+}
 
 export default Home;
