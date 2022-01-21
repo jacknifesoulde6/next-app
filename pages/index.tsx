3fa146da5a703928b6e01fc01ee46836910fcdbe
jacknifesoulde6 <jacknifesoulde6@gmail.com>
2022-01-21 10:26:21 +0900

    エラー埋め込み

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  React.useEffect(() => {
+    const func = async () => {
+      const res = await fetch("http://localhost:3001");
+      console.log(res);
+    };
+    func();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
